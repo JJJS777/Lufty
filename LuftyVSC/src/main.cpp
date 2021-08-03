04dedcdf183e8db4ca29e3757690a2cc1371718c
Manuel Hesse <74195863+TheManitu@users.noreply.github.com>
2021-08-03 13:44:38 +0200

    Servocode
    servo.h wird immer noch nicht geladen ... der esp ist nur für den Servo zuständig demnach kriegt er auch nur diesen code nur wlan verknüpfungen muss man noch ergänzen

--- LuftyVSC/src/main.cpp
+++ LuftyVSC/src/main.cpp
@@ -1,119 +1,30 @@
 #include <Arduino.h>
-#include <Wire.h>
-#include <SPI.h>
-#include <DHT.h>
-#include "WiFi.h"
 #include <Servo.h>
 
-#define DHTPIN 18
-#define DHTTYPE DHT11
 
-const char* ssid = "Duckn3t";
-const char* password =  "quack1QUACK4quack1";
-
-//Globale Variable
-float temperature, humidity, pressure;
-int ledPin = 14;
-int buttonPressed = 0; //Variable global def.
-int buttonPin = 26; 
-
-DHT my_sensor( DHTPIN, DHTTYPE);
-
-void ISRbuttonClicked(){
-  buttonPressed = digitalRead(buttonPin);
-}
+int servoPin = 26; 
+Servo servo1;
+#define servo1Pin 26 // Define the NodeMCU pin to attach the Servo
+int position = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-
-  /*WLAN*/
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.println("Connecting to WiFi..");
+  servo1.attach(servo1Pin);
   }
-
-  Serial.println("Connected to the WiFi network");
-
-  pinMode(ledPin, OUTPUT); //Output Pin wird initalisiert, Zahl auf dem Mainboard am gelben Kabel
-  pinMode(buttonPin, INPUT); //Input pin initalisieren
-  attachInterrupt(buttonPin, ISRbuttonClicked, CHANGE);
-  my_sensor.begin();
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  
-  humidity = my_sensor.readHumidity();
-  temperature = my_sensor.readTemperature();
-
-  if (temperature < 19.00 || temperature > 23.00 || humidity < 50.00  ){
-    digitalWrite(ledPin, HIGH);
-    Serial.println("Messung Prüfen -> Knopf Drücken");
-  }
-
-  if ( buttonPressed > 0 ) {
-    Serial.print("Temperatur: ");
-    Serial.print(temperature);
-    Serial.print("°C / Humidity: ");
-    Serial.print(humidity);
-    Serial.println(" %");
-  }
-  
-
-  delay(2000);
-  /*
-  iswindowOpenable(){
-    if(WindSpeed<40||API_temp <28||API_temp > 8|| AirQuality <100)
-  fensterAuf(){
-  }else {
-    println("Fenster kann nicht geöffnet werden da das Wetter nich geeignet ist.")
-    fensterZu(){
-    
-  }
-  }}
-
-  preSleeping(time>23 && time <24){
-    if(temperature > 18){
-      iswindowOpenable()
-    }
-  }
-  
-  
-  checkHumidity(){
-    if(humidity < 20)
-    {
-      humidifier ON
-      iswindowOpenable()
-    }
-    if(humidity >60)
-    {
-      humidifier OFF
-    }
-  }
-  if(temperature > 23){
-    humidifier OFF
-    iswindowOpenable()
-  }
-  if(temperature <20)
-  {
-    checkHumidity
-  }*/
-
-  
-}
-
-Servo servo1;
-#define servo1Pin D2 // Define the NodeMCU pin to attach the Servo
 
 void servoSetup(){
-  servo1.attach(servo1Pin);
-  //potReading = analogRead(A0);
-  servo1Angle = map(potReading, 0, 1023, 0, 180);
-  servo1.write(servo1Angle); 
-  Serial.println(servo1Angle);
-
-
+    for (pos = 0; pos <= 180; pos += 1) { //0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              
+    delay(20);                       
+  }
+    for (pos = 180; pos >= 0; pos -= 1) { 180 degrees to 0 degrees
+      myservo.write(pos);              
+      delay(20);                       
+    }
+    
+  // potReading = analogRead(A0);
+  // servo1Angle = map(potReading, 0, 1023, 0, 180);
+  // servo1.write(servo1Angle); 
+  // delay(20);
+  // Serial.println(servo1Angle);
 }
