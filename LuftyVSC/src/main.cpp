247562f82a9e694776c435abdd04965c84a7f20e
Manuel Hesse <74195863+TheManitu@users.noreply.github.com>
2021-08-10 17:11:36 +0200

    anpassungen

--- LuftyVSC/src/main.cpp
+++ LuftyVSC/src/main.cpp
@@ -1,29 +1,25 @@
 #include <Arduino.h>
 #include <Servo.h>
 
-int servoPin = 26; 
+static const int servoPin = 34;
+
 Servo servo1;
-#define servo1Pin 26 // Define the NodeMCU pin to attach the Servo
-int pos = 0;
 
 void setup() {
-  servo1.attach(servo1Pin);
-  }
+    Serial.begin(115200);
+    servo1.attach(servoPin);
+}
 
-void servoSetup(){
-    for (pos = 0; pos <= 180; pos += 1) { //0 degrees to 180 degrees
-    // in steps of 1 degree
-    servo1.write(pos);              
-    delay(20);                       
-  }
-    for (pos = 180; pos >= 0; pos -= 1) { //180 degrees to 0 degrees
-      servo1.write(pos);              
-      delay(20);                       
+void loop() {
+    for(int posDegrees = 0; posDegrees <= 180; posDegrees++) {
+        servo1.write(posDegrees);
+        Serial.println(posDegrees);
+        delay(20);
     }
 
-  // potReading = analogRead(A0);
-  // servo1Angle = map(potReading, 0, 1023, 0, 180);
-  // servo1.write(servo1Angle); 
-  // delay(20);
-  // Serial.println(servo1Angle);
+    for(int posDegrees = 180; posDegrees >= 0; posDegrees--) {
+        servo1.write(posDegrees);
+        Serial.println(posDegrees);
+        delay(20);
+    }
 }
