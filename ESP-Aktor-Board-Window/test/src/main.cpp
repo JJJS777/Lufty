5b2fce632b52145f530849704343e70a40f91268
Mayess Sammoud (inf1824) <mayess.sammoud@smail.th-koeln.de>
2021-08-17 21:08:47 +0200

    servo README set

--- ESP-Aktor-Board-Window/test/src/main.cpp
+++ ESP-Aktor-Board-Window/test/src/main.cpp
@@ -5,21 +5,35 @@ static const int servoPin = 16;
 
 Servo servo1;
 
-void setup() {
+void setup()
+{
     Serial.begin(115200);
     servo1.attach(servoPin);
 }
 
-void loop() {
-    for(int posDegrees = 0; posDegrees <= 180; posDegrees++) {
+void window_open()
+{
+    for (int posDegrees = 0; posDegrees <= 180; posDegrees++)
+    {
         servo1.write(posDegrees);
         Serial.println(posDegrees);
         delay(20);
     }
+}
+void window_close()
+{
+    for (int posDegrees = 180; posDegrees >= 0; posDegrees--)
+    {
+        servo1.write(posDegrees);
+        Serial.println(posDegrees);
+        delay(20);
+    }
+}
 
-    for(int posDegrees = 180; posDegrees >= 0; posDegrees--) {
-        servo1.write(posDegrees);
-        Serial.println(posDegrees);
-        delay(20);
-    }
+void loop()
+{
+    window_open();
+    delay(2000);
+    window_close();
+    delay(2000);
 }
