c3009c372e9b3a2a5a2f11b14ba7225a2b1625ea
Julian Schiller <julian.schiller@smail.th-koeln.de>
2021-09-09 17:38:44 +0200

    mqtt msg wird entgegen genommen und drauf richtig reagiert, Problem liegt im ansteuern vom servo....

--- ESP-Aktor-Board-Window/window/src/main.cpp
+++ ESP-Aktor-Board-Window/window/src/main.cpp
@@ -8,6 +8,11 @@
 #include <AsyncMqttClient.h>
 
 
+extern "C" {
+	#include "freertos/FreeRTOS.h"
+	#include "freertos/timers.h"
+}
+
 #define MQTT_HOST IPAddress(192, 168, 141, 99)
 #define MQTT_PORT 1883
 #define MQTT_QoS 1
@@ -15,6 +20,7 @@
 
 char mqttCommandMsg = 'c'; //ggf. zu lokaler variable machen
 static const int servoPin = 16;
+int posDegrees = 0;
 const char *ssid = "Duckn3t";
 const char *password = "quack1QUACK4quack1";
 
@@ -110,24 +116,33 @@ void onMqttPublish(uint16_t packetId)
 
 void window_open()
 {
-    for (int posDegrees = 0; posDegrees <= 180; posDegrees++)
+    Serial.println(posDegrees);
+    for (posDegrees = 0; posDegrees <= 180; posDegrees += 10 )
     {
         servo1.write(posDegrees);
     }
     Serial.println("... Fenster öffnet sich ...");
-    delay(1000);
+    Serial.println(posDegrees);
+    // delay(1000);
 }
 
 void window_close()
 {
-    for (int posDegrees = 180; posDegrees >= 0; posDegrees--)
-    {
-        servo1.write(posDegrees);
-    }
+
+    // if (posDegrees == 180) {
+    //     Serial.println("Fenster ist bereits geschlossen!");
+    // } else {
+    //     for (int posDegrees = 180; posDegrees >= 0; posDegrees += 10) {
+    //         servo1.write(posDegrees);
+    //     }
+        
+    // }
     Serial.println("... Fenster schließt sich ... ");
-    delay(1000);
+    // delay(1000);
 }
 
+
+/*MQTT MSH auslesen und darauf regieren*/
 void onMqttMessage(char *topic, char *payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total)
 {
     for (size_t i = 0; i < len; ++i)
@@ -137,6 +152,9 @@ void onMqttMessage(char *topic, char *payload, AsyncMqttClientMessageProperties 
 
     if(mqttCommandMsg == '1'){ window_open(); }
     if(mqttCommandMsg == '0'){ window_close(); }
+
+    unsigned long currentMillis = millis();
+    Serial.println(currentMillis);
 }
 
 
@@ -162,18 +180,147 @@ void setup()
 
     /*mit WiFi verbindne durch das aufrufen der connectToWifi-Fkt*/
     connectToWifi();
+
+    /*Bei Broker auf Topic Subscriben*/
+    uint16_t packetIdSubWindow = mqttClient.subscribe(MQTT_SUB_WINDOW, MQTT_QoS);
+    Serial.println(packetIdSubWindow);
+    mqttClient.onMessage(onMqttMessage);
 }
 
 void loop()
 {
-    unsigned long currentMillis = millis();
-    // Every X number of seconds (interval = 10 seconds)
-    if (currentMillis - previousMills >= interval)
-    {
-        // Save the last time a new reading was published
-        previousMills = currentMillis;
-        uint16_t packetIdSubWindow = mqttClient.subscribe(MQTT_SUB_WINDOW, MQTT_QoS);
-        Serial.println(packetIdSubWindow);
-        mqttClient.onMessage(onMqttMessage);
-    }
+
+    // // Every X number of seconds (interval = 10 seconds)
+    // if (currentMillis - previousMills >= interval)
+    // {
+    //     // Save the last time a new reading was published
+    //     previousMills = currentMillis;
+    // }
 }
+
+
+// /*TEST START*/
+// #define WIFI_SSID "Duckn3t"
+// #define WIFI_PASSWORD "quack1QUACK4quack1"
+
+// #define MQTT_HOST IPAddress(192, 168, 1, 10)
+// #define MQTT_PORT 1883
+
+// AsyncMqttClient mqttClient;
+// TimerHandle_t mqttReconnectTimer;
+// TimerHandle_t wifiReconnectTimer;
+
+// void connectToWifi() {
+//   Serial.println("Connecting to Wi-Fi...");
+//   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+// }
+
+// void connectToMqtt() {
+//   Serial.println("Connecting to MQTT...");
+//   mqttClient.connect();
+// }
+
+// void WiFiEvent(WiFiEvent_t event) {
+//     Serial.printf("[WiFi-event] event: %d\n", event);
+//     switch(event) {
+//     case SYSTEM_EVENT_STA_GOT_IP:
+//         Serial.println("WiFi connected");
+//         Serial.println("IP address: ");
+//         Serial.println(WiFi.localIP());
+//         connectToMqtt();
+//         break;
+//     case SYSTEM_EVENT_STA_DISCONNECTED:
+//         Serial.println("WiFi lost connection");
+//         xTimerStop(mqttReconnectTimer, 0); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
+//         xTimerStart(wifiReconnectTimer, 0);
+//         break;
+//     }
+// }
+
+// void onMqttConnect(bool sessionPresent) {
+//   Serial.println("Connected to MQTT.");
+//   Serial.print("Session present: ");
+//   Serial.println(sessionPresent);
+//   uint16_t packetIdSub = mqttClient.subscribe("test/lol", 2);
+//   Serial.print("Subscribing at QoS 2, packetId: ");
+//   Serial.println(packetIdSub);
+//   mqttClient.publish("test/lol", 0, true, "test 1");
+//   Serial.println("Publishing at QoS 0");
+//   uint16_t packetIdPub1 = mqttClient.publish("test/lol", 1, true, "test 2");
+//   Serial.print("Publishing at QoS 1, packetId: ");
+//   Serial.println(packetIdPub1);
+//   uint16_t packetIdPub2 = mqttClient.publish("test/lol", 2, true, "test 3");
+//   Serial.print("Publishing at QoS 2, packetId: ");
+//   Serial.println(packetIdPub2);
+// }
+
+// void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
+//   Serial.println("Disconnected from MQTT.");
+
+//   if (WiFi.isConnected()) {
+//     xTimerStart(mqttReconnectTimer, 0);
+//   }
+// }
+
+// void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
+//   Serial.println("Subscribe acknowledged.");
+//   Serial.print("  packetId: ");
+//   Serial.println(packetId);
+//   Serial.print("  qos: ");
+//   Serial.println(qos);
+// }
+
+// void onMqttUnsubscribe(uint16_t packetId) {
+//   Serial.println("Unsubscribe acknowledged.");
+//   Serial.print("  packetId: ");
+//   Serial.println(packetId);
+// }
+
+// void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
+//   Serial.println("Publish received.");
+//   Serial.print("  topic: ");
+//   Serial.println(topic);
+//   Serial.print("  qos: ");
+//   Serial.println(properties.qos);
+//   Serial.print("  dup: ");
+//   Serial.println(properties.dup);
+//   Serial.print("  retain: ");
+//   Serial.println(properties.retain);
+//   Serial.print("  len: ");
+//   Serial.println(len);
+//   Serial.print("  index: ");
+//   Serial.println(index);
+//   Serial.print("  total: ");
+//   Serial.println(total);
+// }
+
+// void onMqttPublish(uint16_t packetId) {
+//   Serial.println("Publish acknowledged.");
+//   Serial.print("  packetId: ");
+//   Serial.println(packetId);
+// }
+
+// void setup() {
+//   Serial.begin(9600);
+//   Serial.println();
+//   Serial.println();
+
+//   mqttReconnectTimer = xTimerCreate("mqttTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToMqtt));
+//   wifiReconnectTimer = xTimerCreate("wifiTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToWifi));
+
+//   WiFi.onEvent(WiFiEvent);
+
+//   mqttClient.onConnect(onMqttConnect);
+//   mqttClient.onDisconnect(onMqttDisconnect);
+//   mqttClient.onSubscribe(onMqttSubscribe);
+//   mqttClient.onUnsubscribe(onMqttUnsubscribe);
+//   mqttClient.onMessage(onMqttMessage);
+//   mqttClient.onPublish(onMqttPublish);
+//   mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+
+//   connectToWifi();
+// }
+
+// void loop() {
+// }
+// /*TEST ENDE*/
